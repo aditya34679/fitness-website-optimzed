182719dd6fac0b9951a70d974c31f84187a4f431
aditya34679 <140746830+aditya34679@users.noreply.github.com>
2025-04-22 18:27:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
 const CACHE_NAME = "fitness-cache-v1";
 const urlsToCache = [
-"/",
+  "/",
   "/home.html",
-  "/style.css",
-  "/images/fit1.jpg",
-  "/images/fit2.jpg",
-  "/images/FIT3.jpg",
-  "/images/fit4.jpg",
-  "/images/fit5.jpg",
-  "/images/fit6.jpg",
-
-  "/images/fit7.jpg",
-  "/images/fit8.jpg",
-  "/images/fit9.jpg",
-  "/images/fit11.jpg",
-  "/images/image1.webp",
-  "/images/image2.webp",
-  "/images/IMG1.png",
-  "/images/img2.jpg",
-  "/images/img3.jpg",
   "/about.html",
   "/BMI.html",
   "/buynow.html",
@@ -27,16 +10,32 @@ const urlsToCache = [
   "/chat.html",
   "/diet.html",
   "/feedback.html",
-  "/fitness center.html",
+  "/fitness-center.html", // renamed
   "/item.html",
   "/login.html",
   "/product.html",
   "/service.html",
   "/signup.html",
   "/social.html",
-  "/script.js",
+  "/faq.html",
   "/offline.html",
-  "/faq.html"
+  "/style.css",
+  "/script.js",
+  "/images/fit1.jpg",
+  "/images/fit2.jpg",
+  "/images/FIT3.jpg",
+  "/images/fit4.jpg",
+  "/images/fit5.jpg",
+  "/images/fit6.jpg",
+  "/images/fit7.jpg",
+  "/images/fit8.jpg",
+  "/images/fit9.jpg",
+  "/images/fit11.jpg",
+  "/images/image1.webp",
+  "/images/image2.webp",
+  "/images/IMG1.png",
+  "/images/img2.jpg",
+  "/images/img3.jpg"
 ];
 
 self.addEventListener("install", event => {
@@ -45,12 +44,35 @@ self.addEventListener("install", event => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("/offline.html");
+          }
+        })
+      );
     })
   );
 });
