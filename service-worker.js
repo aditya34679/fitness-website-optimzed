af2a2ae82dadbf6fd021d674b7729f93aaced7d5
aditya34679 <140746830+aditya34679@users.noreply.github.com>
2025-04-22 23:34:28 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,84 +1,21 @@
 const CACHE_NAME = "fitness-cache-v1";
+
 const urlsToCache = [
-  "/",
-  "/home.html",
-  "/style.css",
-  "/images/fit1.jpg",
-  "/images/fit2.jpg",
-  "/images/fit3.jpg",  // Make sure this matches the actual file name
-  "/images/fit4.jpg",
-  "/images/fit5.jpg",
-  "/images/fit6.jpg",
-  "/images/fit7.jpg",
-  "/images/fit8.jpg",
-  "/images/fit9.jpg",
-  "/images/fit11.jpg",
-  "/images/image1.webp",
-  "/images/image2.webp",
-  "/images/IMG1.png",
-  "/images/img2.jpg",
-  "/images/img3.jpg",
-  "/about.html",
-  "/BMI.html",
-  "/buynow.html",
-  "/cart.html",
-  "/contact.html",
-  "/chat.html",
-  "/diet.html",
-  "/feedback.html",
-  "/fitness-center.html",
-  "/item.html",
-  "/login.html",
-  "/product.html",
-  "/service.html",
-  "/signup.html",
-  "/social.html",
-  "/script.js",
-  "/faq.html",
+  "/", "/home.html", "/style.css",
+  "/images/fit1.jpg", "/images/fit2.jpg", "/images/fit3.jpg", "/images/fit4.jpg",
+  "/images/fit5.jpg", "/images/fit6.jpg", "/images/fit7.jpg", "/images/fit8.jpg",
+  "/images/fit9.jpg", "/images/fit11.jpg",
+  "/images/image1.webp", "/images/image2.webp", "/images/IMG1.png",
+  "/images/img2.jpg", "/images/img3.jpg",
+  "/about.html", "/BMI.html", "/buynow.html", "/cart.html",
+  "/contact.html", "/chat.html", "/diet.html", "/feedback.html",
+  "/fitness-center.html", // consider renaming from "fitness center.html"
+  "/item.html", "/login.html", "/product.html", "/service.html",
+  "/signup.html", "/social.html", "/faq.html", "/script.js",
   "/offline.html"
 ];
 
-const CACHE_NAME = "fitness-cache-v1";
-const urlsToCache = [
-  "/",
-  "/home.html",
-  "/style.css",
-  "/images/fit1.jpg",
-  "/images/fit2.jpg",
-  "/images/FIT3.jpg",
-  "/images/fit4.jpg",
-  "/images/fit5.jpg",
-  "/images/fit6.jpg",
-  "/images/fit7.jpg",
-  "/images/fit8.jpg",
-  "/images/fit9.jpg",
-  "/images/fit11.jpg",
-  "/images/image1.webp",
-  "/images/image2.webp",
-  "/images/IMG1.png",
-  "/images/img2.jpg",
-  "/images/img3.jpg",
-  "/about.html",
-  "/BMI.html",
-  "/buynow.html",
-  "/cart.html",
-  "/contact.html",
-  "/chat.html",
-  "/diet.html",
-  "/feedback.html",
-  "/fitness-center.html",
-  "/item.html",
-  "/login.html",
-  "/product.html",
-  "/service.html",
-  "/signup.html",
-  "/social.html",
-  "/script.js",
-  "/faq.html",
-  "/offline.html"
-];
-
-// Install
+// Install event
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -89,7 +26,7 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-// Activate
+// Activate event
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -105,7 +42,7 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-// Fetch (this is the part you asked about)
+// Fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
